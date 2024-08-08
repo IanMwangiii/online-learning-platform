088ff1f2fdb5d660cac5ad4b58735c8c26da9eca
Griffin798 <griffo@DESKTOP-QM8LH4N.>
2024-08-08 09:21:45 +0300

    ...

--- server/wsgi.py
+++
@@ -1,3 +0,0 @@
-job = JobHadoopTajoQuery('JobHadoopTajoQuerySample',
-                         connection_profile='CP',
-                         open_query='SELECT %%firstParamName AS VAR1 \n FROM DUMMY \n ORDER BY \t VAR1 DESC')
