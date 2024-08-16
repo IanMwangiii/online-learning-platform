bde92b36af00bc1c86e30044d1fe2e5d9bbabbf9
Ian <ian.mwangi1@student.moringaschool.com>
2024-08-16 10:34:03 +0300

    Deployment

---
+++ server/wsgi.py
@@ -0,0 +1,4 @@
+from app import app  # Import the Flask application instance
+
+if __name__ == "__main__":
+    app.run()  # This line is only used when running the application directly, not with Gunicorn
